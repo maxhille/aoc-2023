764c7e656439b29643f8933cc706c8422fbbbc60
Max Hille <mh@lambdasoup.com>
2023-12-15 20:56:27 +0100

    Day 12 - Part 2

--- days/Day12.elm
+++ days/Day12.elm
@@ -1,6 +1,7 @@
-module Day12 exposing (..)
+module Day12 exposing (Spring(..), calculatePart1, calculatePart2, expand, parser, puzzle, recordParser)
 
-import List.Extra exposing (elemIndex, setAt, updateAt)
+import Dict exposing (Dict)
+import List.Extra exposing (intercalate)
 import Parser exposing ((|.), (|=), Parser, Step(..), Trailing(..), andThen, int, loop, map, oneOf, succeed, symbol)
 import Puzzle exposing (Puzzle)
 
@@ -25,95 +26,251 @@ calculatePart1 : String -> Result Error Int
 calculatePart1 input =
     Parser.run parser input
         |> Result.mapError Parser.deadEndsToString
-        |> Result.map (List.map countArrangements)
+        |> Result.map (List.map countWithTries)
         |> Result.map List.sum
 
 
-countArrangements : Record -> Int
-countArrangements record =
-    countArrangementsHelp [ record ]
+calculatePart2 : String -> Result Error Int
+calculatePart2 input =
+    Parser.run parser input
+        |> Result.mapError Parser.deadEndsToString
+        |> Result.map (List.map expand)
+        |> Result.map (List.map countWithTries)
+        |> Result.map List.sum
 
 
-countArrangementsHelp : List Record -> Int
-countArrangementsHelp records =
-    if records |> List.any (.springs >> List.member Unknown) |> not then
-        records
-            |> List.filter valid
-            |> List.length
+expand : Record -> Record
+expand record =
+    { springs = record.springs |> List.repeat 5 |> intercalate [ Unknown ]
+    , groups = record.groups |> List.repeat 5 |> List.concat
+    }
+
+
+type alias RecordTry =
+    { springs : List Spring
+    , groups : List Int
+    , grouping : Grouping
+    }
+
+
+type Grouping
+    = CanStartGroup
+    | InGroup
+    | Closed
+
+
+countWithTries : Record -> Int
+countWithTries { springs, groups } =
+    countWithTriesHelp Dict.empty
+        { springs = springs
+        , groups = groups
+        , grouping = CanStartGroup
+        }
+        |> Tuple.second
+
+
+type alias Key =
+    ( List Int, List Int, Int )
+
+
+groupingToInt : Grouping -> Int
+groupingToInt grouping =
+    case grouping of
+        CanStartGroup ->
+            0
+
+        InGroup ->
+            1
+
+        Closed ->
+            2
+
+
+springsToInt : List Spring -> List Int
+springsToInt =
+    List.map
+        (\spring ->
+            case spring of
+                Damaged ->
+                    0
+
+                Working ->
+                    1
+
+                Unknown ->
+                    2
+        )
+
+
+key : RecordTry -> Key
+key record =
+    ( springsToInt record.springs, record.groups, groupingToInt record.grouping )
+
+
+countWithTriesHelp : Dict Key Int -> RecordTry -> ( Dict Key Int, Int )
+countWithTriesHelp dict record =
+    case Dict.get (key record) dict of
+        Just int ->
+            ( dict, int )
+
+        Nothing ->
+            (\( resultDict, result ) -> ( Dict.insert (key record) result resultDict, result )) <|
+                case reduce record of
+                    Completed ->
+                        ( dict, 1 )
+
+                    Impossible ->
+                        ( dict, 0 )
+
+                    Continue reducedRecord ->
+                        let
+                            ( damagedDict, damagedResult ) =
+                                case tryDamaged reducedRecord of
+                                    Completed ->
+                                        ( dict, 1 )
+
+                                    Impossible ->
+                                        ( dict, 0 )
+
+                                    Continue continueRecord ->
+                                        countWithTriesHelp dict continueRecord
+
+                            ( workingDict, workingResult ) =
+                                case tryWorking reducedRecord of
+                                    Completed ->
+                                        ( damagedDict, 1 )
+
+                                    Impossible ->
+                                        ( damagedDict, 0 )
+
+                                    Continue continueRecord ->
+                                        countWithTriesHelp damagedDict continueRecord
+                        in
+                        ( workingDict, damagedResult + workingResult )
+
+
+type Try
+    = Completed
+    | Impossible
+    | Continue RecordTry
+
+
+reduce : RecordTry -> Try
+reduce record =
+    case ( record.springs, record.groups ) of
+        ( spring :: springs, [] ) ->
+            case spring of
+                Unknown ->
+                    reduce
+                        { springs = springs
+                        , groups = []
+                        , grouping = CanStartGroup
+                        }
+
+                Damaged ->
+                    Impossible
+
+                Working ->
+                    reduce
+                        { springs = springs
+                        , groups = []
+                        , grouping = CanStartGroup
+                        }
+
+        ( spring :: springs, group :: groups ) ->
+            case spring of
+                Unknown ->
+                    Continue record
+
+                Damaged ->
+                    if record.grouping == Closed then
+                        Impossible
+
+                    else if group - 1 == 0 then
+                        reduce
+                            { springs = springs
+                            , groups = groups
+                            , grouping = Closed
+                            }
+
+                    else
+                        reduce
+                            { springs = springs
+                            , groups = (group - 1) :: groups
+                            , grouping = InGroup
+                            }
+
+                Working ->
+                    if record.grouping == InGroup then
+                        Impossible
+
+                    else
+                        reduce
+                            { springs = springs
+                            , groups = group :: groups
+                            , grouping = CanStartGroup
+                            }
+
+        ( [], [] ) ->
+            Completed
+
+        ( [], _ :: _ ) ->
+            Impossible
+
+
+tryDamaged : RecordTry -> Try
+tryDamaged record =
+    case ( record.springs, record.groups ) of
+        ( _ :: springs, group :: groups ) ->
+            if record.grouping == Closed then
+                Impossible
+
+            else if group - 1 == 0 then
+                Continue
+                    { springs = springs
+                    , groups = groups
+                    , grouping = Closed
+                    }
+
+            else
+                Continue
+                    { springs = springs
+                    , groups = (group - 1) :: groups
+                    , grouping = InGroup
+                    }
+
+        ( [], [] ) ->
+            Completed
+
+        ( [], _ :: _ ) ->
+            Impossible
+
+        ( _ :: _, [] ) ->
+            Impossible
+
+
+tryWorking : RecordTry -> Try
+tryWorking record =
+    if record.grouping == InGroup then
+        Impossible
 
     else
-        let
-            newRecords =
-                List.foldl
-                    (\record acc ->
-                        let
-                            record1 =
-                                replaceFirstUnkownWith Working record
+        case ( record.springs, record.groups ) of
+            ( _ :: springs, group :: groups ) ->
+                Continue
+                    { springs = springs
+                    , groups = group :: groups
+                    , grouping = CanStartGroup
+                    }
 
-                            record2 =
-                                replaceFirstUnkownWith Damaged record
-                        in
-                        record1 :: record2 :: acc
-                    )
-                    []
-                    records
-        in
-        countArrangementsHelp newRecords
+            ( [], [] ) ->
+                Completed
 
+            ( [], _ :: _ ) ->
+                Impossible
 
-valid : Record -> Bool
-valid record =
-    record.springs
-        |> List.foldl
-            (\spring acc ->
-                case spring of
-                    Damaged ->
-                        case acc.aggregating of
-                            Just count ->
-                                { acc | aggregating = Just <| count + 1 }
-
-                            Nothing ->
-                                { acc | aggregating = Just 1 }
-
-                    _ ->
-                        case acc.aggregating of
-                            Just count ->
-                                { acc
-                                    | groups = count :: acc.groups
-                                    , aggregating = Nothing
-                                }
-
-                            Nothing ->
-                                acc
-            )
-            { aggregating = Nothing, groups = [] }
-        |> (\{ aggregating, groups } ->
-                case aggregating of
-                    Just count ->
-                        count :: groups
-
-                    Nothing ->
-                        groups
-           )
-        |> List.reverse
-        |> (==) record.groups
-
-
-replaceFirstUnkownWith : Spring -> Record -> Record
-replaceFirstUnkownWith spring record =
-    let
-        maybeIndex =
-            elemIndex Unknown record.springs
-
-        updatedSprings =
-            case maybeIndex of
-                Just i ->
-                    setAt i spring record.springs
-
-                Nothing ->
-                    record.springs
-    in
-    { record | springs = updatedSprings }
+            ( _ :: _, [] ) ->
+                Impossible
 
 
 parser : Parser (List Record)
@@ -178,5 +335,5 @@ puzzle : Puzzle
 puzzle =
     { validate = Parser.run parser >> Result.map (\_ -> "could not parse") >> Result.mapError Parser.deadEndsToString
     , calculatePart1 = calculatePart1
-    , calculatePart2 = \_ -> Err "not implemented"
+    , calculatePart2 = calculatePart2
     }
--- tests/Day12Test.elm
+++ tests/Day12Test.elm
@@ -25,6 +25,22 @@ suite =
                 Expect.equal
                     (Ok 21)
                     (calculatePart1 input)
+        , test "Part 2 Example" <|
+            \_ ->
+                let
+                    input =
+                        """
+                        ???.### 1,1,3
+                        .??..??...?##. 1,1,3
+                        ?#?#?#?#?#?#?#? 1,3,1,6
+                        ????.#...#... 4,1,1
+                        ????.######..#####. 1,6,5
+                        ?###???????? 3,2,1
+                        """
+                in
+                Expect.equal
+                    (Ok 525152)
+                    (calculatePart2 input)
         , test "Parse Records" <|
             \_ ->
                 let
@@ -41,4 +57,21 @@ suite =
                         ]
                     )
                     (Parser.run parser input)
+        , test "expand" <|
+            \_ ->
+                let
+                    record =
+                        { springs = [ Unknown, Unknown, Unknown, Working, Damaged, Damaged, Damaged ]
+                        , groups = [ 1, 1, 3 ]
+                        }
+
+                    expected =
+                        "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3"
+                            |> Parser.run recordParser
+                            |> Result.toMaybe
+                            |> Maybe.withDefault { springs = [], groups = [] }
+                in
+                Expect.equal
+                    expected
+                    (expand record)
         ]
