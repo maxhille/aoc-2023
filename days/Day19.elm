704ee1651dd9a9632ab67920aa6fb9e6f634a1be
Max Hille <mh@lambdasoup.com>
2023-12-29 08:30:40 +0100

    Day 19 - Part 2

--- days/Day19.elm
+++ days/Day19.elm
@@ -1,4 +1,4 @@
-module Day19 exposing (Condition(..), To(..), calculatePart1, calculatePart2, parser, puzzle)
+module Day19 exposing (Condition(..), Split(..), To(..), calculatePart1, calculatePart2, parser, puzzle, split)
 
 import Dict exposing (Dict)
 import Parser exposing ((|.), (|=), Parser, Trailing(..), int, oneOf, spaces, succeed, symbol, variable)
@@ -18,6 +18,18 @@ type alias Part =
     , a : Int
     , s : Int
     }
+
+
+type alias PartRange =
+    { x : Range
+    , m : Range
+    , a : Range
+    , s : Range
+    }
+
+
+type alias Range =
+    { from : Int, to : Int }
 
 
 type alias Rule =
@@ -42,61 +54,98 @@ type To
 
 calculatePart1 : System -> Result String Int
 calculatePart1 system =
-    process system.rules system.parts
-        |> Result.map (List.map (\{ x, m, a, s } -> x + m + a + s) >> List.sum)
+    let
+        toRange int =
+            { from = int, to = int }
+
+        partRanges =
+            system.parts
+                |> List.map
+                    (\{ x, m, a, s } ->
+                        { x = toRange x
+                        , m = toRange m
+                        , a = toRange a
+                        , s = toRange s
+                        }
+                    )
+    in
+    process system.rules partRanges
+        |> Result.map
+            (List.map (\{ x, m, a, s } -> x.from + m.from + a.from + s.from)
+                >> List.sum
+            )
 
 
 calculatePart2 : System -> Result String Int
-calculatePart2 _ =
-    Err "not implemented"
+calculatePart2 system =
+    process system.rules
+        [ { x = { from = 1, to = 4000 }
+          , m = { from = 1, to = 4000 }
+          , a = { from = 1, to = 4000 }
+          , s = { from = 1, to = 4000 }
+          }
+        ]
+        |> Result.map
+            (List.map (\{ x, m, a, s } -> (x.to - x.from + 1) * (m.to - m.from + 1) * (a.to - a.from + 1) * (s.to - s.from + 1))
+                >> List.sum
+            )
 
 
-process : List Rule -> List Part -> Result String (List Part)
+process : List Rule -> List PartRange -> Result String (List PartRange)
 process rules =
     let
         dict =
             rules |> List.map (\rule -> ( rule.id, rule )) |> Dict.fromList
     in
-    List.map (\part -> processHelp dict part (Next "in"))
-        >> List.foldr
-            (\partResult partsResult ->
-                case ( partsResult, partResult ) of
-                    ( Ok parts, Ok maybePart ) ->
-                        case maybePart of
-                            Just part ->
-                                Ok <| part :: parts
-
-                            Nothing ->
-                                Ok parts
-
-                    ( _, Err error ) ->
-                        Err error
-
-                    ( Err error, _ ) ->
-                        Err error
-            )
-            (Ok [])
+    List.map (\part -> { part = part, to = Next "in" })
+        >> processHelp dict
 
 
-processHelp : Dict String Rule -> Part -> To -> Result String (Maybe Part)
-processHelp rules part to =
-    case to of
-        Accept ->
-            Ok <| Just part
+processHelp : Dict String Rule -> List { part : PartRange, to : To } -> Result String (List PartRange)
+processHelp rules parts =
+    if parts |> List.all (\{ to } -> to == Accept) then
+        Ok <| List.map .part parts
 
-        Reject ->
-            Ok Nothing
+    else
+        case
+            List.foldl
+                (\{ part, to } acc ->
+                    case acc of
+                        Err err ->
+                            Err err
 
-        Next ruleId ->
-            case Dict.get ruleId rules of
-                Nothing ->
-                    Err "unknown rule ID"
+                        Ok parts_ ->
+                            case to of
+                                Reject ->
+                                    Ok parts_
 
-                Just rule ->
-                    apply rule.conditions part |> Result.andThen (processHelp rules part)
+                                Accept ->
+                                    Ok <| { part = part, to = to } :: parts_
+
+                                Next ruleId ->
+                                    case Dict.get ruleId rules of
+                                        Nothing ->
+                                            Err "could not find rule by ID"
+
+                                        Just rule ->
+                                            case apply rule.conditions part of
+                                                Ok newParts ->
+                                                    Ok <| List.append newParts parts_
+
+                                                Err err ->
+                                                    Err err
+                )
+                (Ok [])
+                parts
+        of
+            Err err ->
+                Err err
+
+            Ok parts_ ->
+                processHelp rules parts_
 
 
-apply : List Condition -> Part -> Result String To
+apply : List Condition -> PartRange -> Result String (List { part : PartRange, to : To })
 apply conditions part =
     case conditions of
         [] ->
@@ -105,7 +154,7 @@ apply conditions part =
         [ condition ] ->
             case condition of
                 Default to ->
-                    Ok to
+                    Ok <| [ { part = part, to = to } ]
 
                 _ ->
                     Err "last condition was no default"
@@ -113,35 +162,91 @@ apply conditions part =
         condition :: remainder ->
             case condition of
                 X order int to ->
-                    if compare part.x int == order then
-                        Ok to
+                    case split order int part.x of
+                        Both toRange nextRange ->
+                            Result.map2 (::)
+                                (Ok <| { part = { part | x = toRange }, to = to })
+                                (apply remainder { part | x = nextRange })
 
-                    else
-                        apply remainder part
+                        OnlyTo toRange ->
+                            Ok <| [ { part = { part | x = toRange }, to = to } ]
+
+                        OnlyNext nextRange ->
+                            apply remainder { part | x = nextRange }
 
                 M order int to ->
-                    if compare part.m int == order then
-                        Ok to
+                    case split order int part.m of
+                        Both toRange nextRange ->
+                            Result.map2 (::)
+                                (Ok <| { part = { part | m = toRange }, to = to })
+                                (apply remainder { part | m = nextRange })
 
-                    else
-                        apply remainder part
+                        OnlyTo toRange ->
+                            Ok <| [ { part = { part | m = toRange }, to = to } ]
+
+                        OnlyNext nextRange ->
+                            apply remainder { part | m = nextRange }
 
                 A order int to ->
-                    if compare part.a int == order then
-                        Ok to
+                    case split order int part.a of
+                        Both toRange nextRange ->
+                            Result.map2 (::)
+                                (Ok <| { part = { part | a = toRange }, to = to })
+                                (apply remainder { part | a = nextRange })
 
-                    else
-                        apply remainder part
+                        OnlyTo toRange ->
+                            Ok <| [ { part = { part | a = toRange }, to = to } ]
+
+                        OnlyNext nextRange ->
+                            apply remainder { part | a = nextRange }
 
                 S order int to ->
-                    if compare part.s int == order then
-                        Ok to
+                    case split order int part.s of
+                        Both toRange nextRange ->
+                            Result.map2 (::)
+                                (Ok <| { part = { part | s = toRange }, to = to })
+                                (apply remainder { part | s = nextRange })
 
-                    else
-                        apply remainder part
+                        OnlyTo toRange ->
+                            Ok <| [ { part = { part | s = toRange }, to = to } ]
+
+                        OnlyNext nextRange ->
+                            apply remainder { part | s = nextRange }
 
                 Default to ->
-                    Ok to
+                    Ok <| [ { part = part, to = to } ]
+
+
+type Split
+    = Both Range Range
+    | OnlyNext Range
+    | OnlyTo Range
+
+
+split : Order -> Int -> Range -> Split
+split order int range =
+    if order == GT then
+        if range.from > int then
+            OnlyTo range
+
+        else if range.to <= int then
+            OnlyNext range
+
+        else
+            Both { from = int + 1, to = range.to } { from = range.from, to = int }
+
+    else if range.to < int then
+        OnlyTo range
+
+    else if range.from >= int then
+        OnlyNext range
+
+    else
+        Both { from = range.from, to = int - 1 } { from = int, to = range.to }
+
+
+
+-- compare part.x int == order then
 
 
 parser : Parser System
--- tests/Day19Test.elm
+++ tests/Day19Test.elm
@@ -16,24 +16,24 @@ suite =
                     input =
                         sanitize <|
                             """
-                            px{a<2006:qkq,m>2090:A,rfg}
-                            pv{a>1716:R,A}
-                            lnx{m>1548:A,A}
-                            rfg{s<537:gd,x>2440:R,A}
-                            qs{s>3448:A,lnx}
-                            qkq{x<1416:A,crn}
-                            crn{x>2662:A,R}
-                            in{s<1351:px,qqz}
-                            qqz{s>2770:qs,m<1801:hdj,R}
-                            gd{a>3333:R,R}
-                            hdj{m>838:A,pv}
+                                 px{a<2006:qkq,m>2090:A,rfg}
+                                 pv{a>1716:R,A}
+                                 lnx{m>1548:A,A}
+                                 rfg{s<537:gd,x>2440:R,A}
+                                 qs{s>3448:A,lnx}
+                                 qkq{x<1416:A,crn}
+                                 crn{x>2662:A,R}
+                                 in{s<1351:px,qqz}
+                                 qqz{s>2770:qs,m<1801:hdj,R}
+                                 gd{a>3333:R,R}
+                                 hdj{m>838:A,pv}
 
-                            {x=787,m=2655,a=1222,s=2876}
-                            {x=1679,m=44,a=2067,s=496}
-                            {x=2036,m=264,a=79,s=2244}
-                            {x=2461,m=1339,a=466,s=291}
-                            {x=2127,m=1623,a=2188,s=1013}
-                            """
+                                 {x=787,m=2655,a=1222,s=2876}
+                                 {x=1679,m=44,a=2067,s=496}
+                                 {x=2036,m=264,a=79,s=2244}
+                                 {x=2461,m=1339,a=466,s=291}
+                                 {x=2127,m=1623,a=2188,s=1013}
+                                 """
                 in
                 Expect.equal
                     (Ok 19114)
@@ -41,6 +41,57 @@ suite =
                         |> Result.mapError Parser.deadEndsToString
                         |> Result.andThen calculatePart1
                     )
+        , test "Part 2 - Example" <|
+            \_ ->
+                let
+                    input =
+                        sanitize <|
+                            """
+                                 px{a<2006:qkq,m>2090:A,rfg}
+                                 pv{a>1716:R,A}
+                                 lnx{m>1548:A,A}
+                                 rfg{s<537:gd,x>2440:R,A}
+                                 qs{s>3448:A,lnx}
+                                 qkq{x<1416:A,crn}
+                                 crn{x>2662:A,R}
+                                 in{s<1351:px,qqz}
+                                 qqz{s>2770:qs,m<1801:hdj,R}
+                                 gd{a>3333:R,R}
+                                 hdj{m>838:A,pv}
+
+                                 {x=787,m=2655,a=1222,s=2876}
+                                 {x=1679,m=44,a=2067,s=496}
+                                 {x=2036,m=264,a=79,s=2244}
+                                 {x=2461,m=1339,a=466,s=291}
+                                 {x=2127,m=1623,a=2188,s=1013}
+                                 """
+                in
+                Expect.equal
+                    (Ok 167409079868000)
+                    (Parser.run parser input
+                        |> Result.mapError Parser.deadEndsToString
+                        |> Result.andThen calculatePart2
+                    )
+        , test "split 1" <|
+            \_ ->
+                Expect.equal
+                    (OnlyTo { from = 100, to = 200 })
+                    (split GT 99 { from = 100, to = 200 })
+        , test "split 2" <|
+            \_ ->
+                Expect.equal
+                    (OnlyNext { from = 100, to = 200 })
+                    (split GT 200 { from = 100, to = 200 })
+        , test "split 3" <|
+            \_ ->
+                Expect.equal
+                    (Both { from = 151, to = 200 } { from = 100, to = 150 })
+                    (split GT 150 { from = 100, to = 200 })
+        , test "split 4" <|
+            \_ ->
+                Expect.equal
+                    (Both { from = 70, to = 99 } { from = 100, to = 200 })
+                    (split LT 100 { from = 70, to = 200 })
         , test "parser" <|
             \_ ->
                 let
